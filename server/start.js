8cd77808cae6bd75dc5597efe0efa2a377d94011
Vivi Wen <hsinweiwen@Hsins-MacBook-Pro.local>
2016-01-05 15:03:29 -0500

    update

--- server/start.js
+++ server/start.js
@@ -2,9 +2,7 @@
 'use strict';
 //require("babel-register");
 require('babel-register')({
-    "presets": [
-      "es2015"
-    ]
+    "presets": ["es2015"]
   });
 //require('babel/register');
 require('./app');
